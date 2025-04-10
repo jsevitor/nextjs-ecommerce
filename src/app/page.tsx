f5a02d3f9829054adcb78a1bc57fa983e67617fe
jsevitor <vitorjseo@gmail.com>
2025-04-10 14:44:24 +0000

    feat: adiciona o componente Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main>
-      <h1>HOME PAGE</h1>
+      <Header />
     </main>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+export default function Header() {
+  return (
+    <header className="container mx-auto h-16 flex justify-between items-center">
+      <nav>
+        <ul className="flex gap-4">
+          <li>Mulheres</li>
+          <li>Homens</li>
+          <li>Crianças</li>
+          <li>Novidades</li>
+        </ul>
+      </nav>
+      <div className="absolute left-1/2 transform -translate-x-1/2 font-bold text-lg">
+        TULOS
+      </div>
+      <div className="flex gap-4">
+        <div>
+          <i className="bi bi-search"></i>
+        </div>
+        <div>
+          <i className="bi bi-bag"></i>
+        </div>
+        <div>
+          <span>Login</span>
+        </div>
+      </div>
+    </header>
+  );
+}
